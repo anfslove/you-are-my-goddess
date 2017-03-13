e791c56ffebfdad50e05aababf1a342a877433d1
Pengcheng <anfslove@gmail.com>
2017-03-13 21:46:08 +0800

    Completed the goddess editor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Goddess {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'You Are My Goddess';
+  goddess: Goddess = {
+    id: 1,
+    name: "Yan Jingjing"
+  }
 }
