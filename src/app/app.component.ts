09aa46b777ea9ee26a4a1ab4a46167f96ca1ee27
Pengcheng <anfslove@gmail.com>
2017-03-15 19:35:00 +0800

    Completed displaying my goddesses.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,26 @@ export class Goddess {
   name: string;
 }
 
+const GODDESSES: Goddess[] = [
+  {id: 1, name: 'Yan Jingjing'},
+  {id: 2, name: 'Tang Wei'},
+  {id: 3, name: 'Tong Liya'},
+  {id: 4, name: 'Sun Li'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'You Are My Goddess';
-  goddess: Goddess = {
-    id: 1,
-    name: "Yan Jingjing"
+  goddesses = GODDESSES;
+  selectedGoddess: Goddess;
+
+  onSelect(goddess: Goddess): void {
+    this.selectedGoddess = goddess;
   }
 }
+
